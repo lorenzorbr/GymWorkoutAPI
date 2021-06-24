5919a4464c25526395906c2f58e2f60398876414
Lorenzo <lorenzorbr@hotmail.com>
2021-06-24 10:01:45 -0300

    fourth day

--- lib/wabanex_web/schema/types/user.ex
+++ lib/wabanex_web/schema/types/user.ex
@@ -1,8 +1,6 @@
 defmodule WabanexWeb.Schema.Types.User do
   use Absinthe.Schema.Notation
 
-  import_types WabanexWeb.Schema.Types.Custom.UUID4
-
   @desc "Logic User Representation"
   object :user do
     field :id, non_null(:uuid4)
@@ -15,7 +13,6 @@ defmodule WabanexWeb.Schema.Types.User do
     field :name, non_null(:string), description: "Users name"
     field :email, non_null(:string), description: "Users email"
     field :password, non_null(:string), description: "Users password"
-
   end
 
 end
